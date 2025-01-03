69f4b05c77eb345f9f17fefdd2a6e7efeab1e11c
Jeremy <010JGL@GMAIL.COM>
2025-01-03 15:51:50 -0500

    homepage finished

--- src/Contact.js
+++ src/Contact.js
@@ -6,93 +6,94 @@ const Contact = () => {
   const { language } = useLanguage(); // Access the current language
 
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        padding: "10px 0", // Reduced padding
-        marginTop: "20px", // Reduced margin
-      }}
-    >
-      <Container>
-        <Grid
-          container
-          spacing={3}
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          {/* Logo (Smaller box) */}
+    <section id="contactSection">
+      <div
+        style={{
+          backgroundColor: "black",
+          padding: "10px 0", // Reduced padding
+          marginTop: "20px", // Reduced margin
+        }}
+      >
+        <Container>
           <Grid
-            item
-            xs={12}
-            sm={3}
-            style={{ display: "flex", justifyContent: "center" }}
+            container
+            spacing={3}
+            alignItems="center"
+            justifyContent="space-between"
           >
-            <Box
-              style={{
-                width: "100px", // Smaller width for the logo
-                height: "30px", // Reduced height for a more compact logo
-                backgroundColor: "gray", // Placeholder for logo
-                borderRadius: "10px",
-              }}
-            >
-              {/* Add your logo here later */}
-            </Box>
-          </Grid>
-
-          {/* Contact Information (Larger box for text) */}
-          <Grid item xs={12} sm={9}>
+            {/* Logo (Smaller box) */}
             <Grid
-              container
-              spacing={3}
-              justifyContent="space-between"
-              alignItems="center"
+              item
+              xs={12}
+              sm={3}
+              style={{ display: "flex", justifyContent: "center" }}
             >
-              {/* Email */}
-              <Grid item xs={12} sm={4} style={{ textAlign: "center" }}>
-                <Typography variant="body2" style={{ color: "white" }}>
-                  {language === "fr" ? "Courriel:" : "Email:"}{" "}
-                  <a
-                    href="mailto:GestionTopsDuTop@gmail.com"
-                    style={{ color: "white" }}
-                  >
-                    GestionTopsDuTop@gmail.com
-                  </a>
-                </Typography>
-              </Grid>
+              <Box
+                style={{
+                  width: "100px", // Smaller width for the logo
+                  height: "30px", // Reduced height for a more compact logo
+                  backgroundColor: "gray", // Placeholder for logo
+                  borderRadius: "10px",
+                }}
+              >
+                {/* Add your logo here later */}
+              </Box>
+            </Grid>
 
-              {/* Location */}
-              <Grid item xs={12} sm={4} style={{ textAlign: "center" }}>
-                <Typography variant="body2" style={{ color: "white" }}>
-                  {language === "fr" ? "Situé à:" : "Located at:"}
-                </Typography>
-                <Typography variant="body2" style={{ color: "white" }}>
-                  Chambly, QC
-                </Typography>
-              </Grid>
+            {/* Contact Information (Larger box for text) */}
+            <Grid item xs={12} sm={9}>
+              <Grid
+                container
+                spacing={3}
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                {/* Email */}
+                <Grid item xs={12} sm={4} style={{ textAlign: "center" }}>
+                  <Typography variant="body2" style={{ color: "white" }}>
+                    {language === "fr" ? "Courriel:" : "Email:"}{" "}
+                    <a
+                      href="mailto:GestionTopsDuTop@gmail.com"
+                      style={{ color: "white" }}
+                    >
+                      GestionTopsDuTop@gmail.com
+                    </a>
+                  </Typography>
+                </Grid>
 
-              {/* Operating Hours */}
-              <Grid item xs={12} sm={4}>
-                <Typography variant="body2" style={{ color: "white" }}>
-                  {language === "fr"
-                    ? "Heures d'ouverture:"
-                    : "Operating Hours:"}{" "}
-                  <br />
-                  {language === "fr"
-                    ? "Lundi-Vendredi: 7h00 - 19h00"
-                    : "Mon-Fri, 7:00 AM - 7:00 PM"}{" "}
-                  <br />
-                  {language === "fr"
-                    ? "Samedi-Dimanche: sur rendez-vous"
-                    : "Sat-Sun, Appointment only"}
-                </Typography>
+                {/* Location */}
+                <Grid item xs={12} sm={4} style={{ textAlign: "center" }}>
+                  <Typography variant="body2" style={{ color: "white" }}>
+                    {language === "fr" ? "Situé à:" : "Located at:"}
+                  </Typography>
+                  <Typography variant="body2" style={{ color: "white" }}>
+                    Chambly, QC
+                  </Typography>
+                </Grid>
+
+                {/* Operating Hours */}
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body2" style={{ color: "white" }}>
+                    {language === "fr"
+                      ? "Heures d'ouverture:"
+                      : "Operating Hours:"}{" "}
+                    <br />
+                    {language === "fr"
+                      ? "Lundi-Vendredi: 7h00 - 19h00"
+                      : "Mon-Fri, 7:00 AM - 7:00 PM"}{" "}
+                    <br />
+                    {language === "fr"
+                      ? "Samedi-Dimanche: sur rendez-vous"
+                      : "Sat-Sun, Appointment only"}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </section>
   );
 };
 
 export default Contact;
-
